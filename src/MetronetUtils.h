9257008da8684722bf53ee3e961ade4605adc088
Dawid Miroyan <dawid.miroyan@student.uatwerpen.be>
2017-03-16 14:05:53 +0000

    Functions file manipulation, created MetronetInputTest.cpp

---
+++ src/MetronetUtils.h
@@ -0,0 +1,19 @@
+//
+// Created by dawid on 16.03.17.
+//
+
+#ifndef PROJECTFMM_METRONETUTILS_H
+#define PROJECTFMM_METRONETUTILS_H
+
+
+#include <iostream>
+
+bool DirectoryExists(const std::string dirname);
+
+bool FileExists(const std::string dirname);
+
+bool FileIsEmpty(const std::string filename);
+
+bool FileCompare(const std::string leftFileName, const std::string rightFileName);
+
+#endif //PROJECTFMM_METRONETUTILS_H
--- src/Metronet_test.cpp
+++ src/Metronet_test.cpp
@@ -5,5 +5,10 @@
  *      Author: sergio
  */
 
-#include "Metronet.h"
+#include <gtest/gtest.h>
 
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
