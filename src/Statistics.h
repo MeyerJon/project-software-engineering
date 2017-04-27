cf65731922d065faa6eba18e13998b078bbfc342
Jonathan.Meyer@student.uantwerpen.be <Jonathan.Meyer@student.uantwerpen.be>
2017-04-27 16:48:39 +0200

    Started work on Statistics

--- src/Statistics.h
+++ src/Statistics.h
@@ -8,12 +8,71 @@
 #include <vector>
 #include <string>
 
-/**
- * \brief Deze klasse houdt alle extra gegevens voor het metronet bij zoals omzet, bezettingsgraad...
- */
-class Statistics {
+
+class StatisticsTram {
+private:
     double omzet;
+    double bezettingsgraad;
+    int aantalGroepen;
+    int aantalPersonen;
+    int aantalFails;
+    StatisticsTram* initCheck;
+public:
+    /**
+     * \brief De lege constructor voor StatisticsTram
+     * \post ENSURE(this->properlyInitialised(), "StatisticsTram is niet in de juiste staat geëindigd na aanroep van de constructor.");
+     */
+     StatisticsTram();
+
+    /**
+     * \brief Geeft de totale omzet van de tram op dit moment.
+     * \pre REQUIRE(this->properlyInitialised(), "StatisticsTram was niet geinitialiseerd bij aanroep van getOmzet.");
+     */
+    double getOmzet() const;
+
+    /**
+     * \brief Geeft de totale bezettingsgraad van de tram op dit moment.
+     * \pre REQUIRE(this->properlyInitialised(), "StatisticsTram was niet geinitialiseerd bij aanroep van getBezettingsgraad.");
+     */
+    double getBezettingsgraad() const;
+
+    /**
+     * \brief Geeft het aantal groepen dat deze tram gebruikt hebben.
+     * \pre REQUIRE(this->properlyInitialised(), "StatisticsTram was niet geinitialiseerd bij aanroep van getAantalGroepen.");
+     */
+    int getAantalGroepen() const;
+
+    /**
+     * \brief Geeft het aantal personen dat deze tram gebruikt hebben.
+     * \pre REQUIRE(this->properlyInitialised(), "StatisticsTram was niet geinitialiseerd bij aanroep van getAantalPersonen.");
+     */
+
+    int getAantalPersonen() const;
+
+    /**
+     * \brief Geeft het aantal keren dat een groep niet kon opstappen.
+     * \pre REQUIRE(this->properlyInitialised(), "StatisticsTram was niet geinitialiseerd bij aanroep van getAantalFails.");
+     */
+
+    int getAantalFails() const;
+
 };
+
+
+class StatisticsStation {
+    int aantalGroepen;
+    int aantalPersonen;
+};
+
+
+class StatisticsMetronet {
+    double totaleOmzet;
+    double totaleBezettingsgraad;
+    int popLijn;
+    int nrAlba;
+    int nrPCC;
+};
+
 
 
 #endif //PSE_STATISTICS_H
